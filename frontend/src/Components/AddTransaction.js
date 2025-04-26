9782a2e8966b34f659ba54f8ea38a3cc55635208
Anmol Kumar <132394800+acpm1221@users.noreply.github.com>
2025-04-27 03:45:12 +0530

    Update AddTransaction.js

--- frontend/src/Components/AddTransaction.js
+++ frontend/src/Components/AddTransaction.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import BASE_URL from './config'; 
+import BASE_URL from '../config'; 
 import './AddTransaction.css';
 
 function AddTransaction({ token, type, onClose, onTransactionAdded }) {
